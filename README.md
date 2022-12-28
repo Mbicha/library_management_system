0e51a4b700780796c51bde3202a5862d1cf0a86d
Mbicha <mbithicharlse@gmail.com>
2022-12-28 20:41:03 +0300

    Worked on Home and borrowed.html halfway

---
+++ README.md
@@ -0,0 +1,13 @@
+## Library Management System
+
+This is a web application for helping librarians keep track of books issued.
+
+# Technologies
+
+HTML
+Tailwindcss
+JavaScript
+Python3 (flask)
+Database (Mysql)
+
+# Folders and directories
---
+++ build/database/create_lbs_database.sql
@@ -0,0 +1,67 @@
+-- CREATES lbs database
+
+CREATE DATABASE IF NOT EXISTS lbs;
+
+USE lbs;
+
+-- CREATE TABLE user
+
+CREATE TABLE IF NOT EXISTS user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email_address VARCHAR(60) UNIQUE,
+    phone VARCHAR(15),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- LIBRARY TABLE
+
+CREATE TABLE IF NOT EXISTS librarian (
+    librarian_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email_address VARCHAR(60) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- BOOK TABLE
+CREATE TABLE IF NOT EXISTS book (
+    isbn_no VARCHAR(20) PRIMARY KEY UNIQUE,
+    bk_title TEXT NOT NULL,
+    bk_authors TEXT NOT NULL,
+    year_published INT,
+    publisher VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ISSUED TABLE
+CREATE TABLE IF NOT EXISTS issued (
+    issued_id INT AUTO_INCREMENT PRIMARY KEY,
+    issued_date DATETIME NOT NULL,
+    book_isbn VARCHAR(20) NOT NULL,
+    librarian_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (book_isbn) 
+        REFERENCES book (isbn_no) 
+        ON UPDATE RESTRICT 
+        ON DELETE CASCADE,
+    FOREIGN KEY (librarian_id) 
+        REFERENCES librarian (librarian_id) 
+        ON UPDATE RESTRICT 
+        ON DELETE CASCADE,
+    FOREIGN KEY (user_id) 
+        REFERENCES user (user_id) 
+        ON UPDATE RESTRICT 
+        ON DELETE CASCADE
+);
+
+-- RETURN TABLE
+
+CREATE TABLE IF NOT EXISTS bk_return (
+    return_id INT AUTO_INCREMENT PRIMARY KEY,
+    issued_id INT NOT NULL,
+    isreturned ENUM('Yes', 'No'),
+    FOREIGN KEY (issued_id) 
+        REFERENCES issued (issued_id) 
+        ON UPDATE RESTRICT 
+        ON DELETE CASCADE
+);
